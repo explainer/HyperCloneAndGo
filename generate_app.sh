9a76e4441c6c65fb43fe9d91f340c5dbcafbb92d
Ken Burgett <keburgett@gmail.com>
2017-06-07 11:50:48 -0700

    added dev log and sqlite db

--- generate_app.sh
+++ generate_app.sh
@@ -3,7 +3,7 @@
 #rails g scaffold History start_time:datetime stop_time:datetime valve_id:integer --force
 #rails g scaffold Sprinkle next_start_time:datetime base_start_time:datetime time_input:string duration:integer valve_id:integer --force
 rails g scaffold Valve name:string gpio_pin:integer cpu2bb_color:string bb_pin:integer bb2relay_color:string relay_module:integer \
-  relay_index:integer relay2valve_color:string cmd:integer base_time:datetime
+  relay_index:integer relay2valve_color:string cmd:integer base_time:datetime --force
 #rails g scaffold WaterManager state:string http_host:string --force
 # rails g app_constants
 # sh development-bounce-db.sh
